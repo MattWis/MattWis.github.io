6fc6c3000079382bc715959925e79ea12b2a9d7f
MattWis <matthew.wismer@students.olin.edu>
2013-10-05 14:46:47 -0400

    Added firebase and basic clicking functionality

--- scripts/game.js
+++ scripts/game.js
@@ -6,6 +6,12 @@ define(['zepto', 'pixi'], function ($, PIXI) {
       , stage: null
       }
     , init: function () {
+					var clicks = new Firebase('https://olinhackmit.firebaseIO.com/clicks');
+
+					clicks.on('child_added', function(snapshot) {
+						console.log(snapshot.val());
+					});
+
           var _g = this
             , renderer = new PIXI.autoDetectRenderer(800, 600)
             , stage = new PIXI.Stage(0x66FF99, true)
@@ -17,6 +23,8 @@ define(['zepto', 'pixi'], function ($, PIXI) {
 
           stage.mousedown = function (e) {
             _g.avatar.position = e.global.clone();
+						console.log('clack');
+						clicks.push({click: 1});
           }
           _g.avatar = new PIXI.Sprite(avatarTexture);
 
