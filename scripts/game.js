682ab5aa2e67fc31a8aa25e99a5cac3cc2541f3b
Julian Ceipek <julian.ceipek@gmail.com>
2013-10-05 14:27:11 -0400

    initial graphics

---
+++ scripts/game.js
@@ -0,0 +1,46 @@
+define(['zepto', 'pixi'], function ($, PIXI) {
+
+    var G = {
+      state: {
+        renderer: null
+      , stage: null
+      }
+    , init: function () {
+          var _g = this
+            , renderer = new PIXI.autoDetectRenderer(800, 600)
+            , stage = new PIXI.Stage(0x66FF99, true)
+            , avatarTexture = PIXI.Texture.fromImage("images/testAvatar.png")
+            , geo = PIXI.Graphics();
+
+          renderer.view.style.display = "block";
+          document.body.appendChild(renderer.view);
+
+          stage.mousedown = function (e) {
+            _g.avatar.position = e.global.clone();
+          }
+          _g.avatar = new PIXI.Sprite(avatarTexture);
+
+          _g.avatar.position.x = 400;
+          _g.avatar.position.y = 300;
+
+          _g.avatar.scale.x = 0.5;
+          _g.avatar.scale.y = 0.5;
+
+          stage.addChild(_g.avatar);
+
+          requestAnimationFrame(_g.animate.bind(_g));
+
+          _g.state.renderer = renderer;
+          _g.state.stage = stage;
+        }
+      , animate: function () {
+          var _g = this;
+          _g.avatar.rotation += 0.1;
+          _g.state.renderer.render(_g.state.stage);
+          requestAnimationFrame(_g.animate.bind(_g));
+        }
+      };
+
+    window.G = G;
+    return G;
+  });
